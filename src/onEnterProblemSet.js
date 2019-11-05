0563560cd8a69d57981efccc8e4e48dc148b972a
EvsChen <chensy1996@sjtu.edu.cn>
2019-11-05 11:51:11 -0500

    Archive

--- src/onEnterProblemSet.js
+++ src/onEnterProblemSet.js
@@ -5,22 +5,18 @@ const mainInterval = setInterval(() => {
   const ths = document.querySelectorAll('th');
   if (ths.length > 0) {
     clearInterval(mainInterval);
-    hide();
+    getDefaultOptions()
+        .then((options) => {
+          if (options[OPTIONS_KEYS.HIDE_DIFFICULTY]) {
+            Array.from(document.querySelectorAll('th')).forEach((th, index) => {
+              const text = th.innerText.toLowerCase().trim();
+              if (text === 'acceptance' || text === 'difficulty') {
+                document.styleSheets[0].insertRule(
+                    `tr td:nth-of-type(${index + 1}) {font-size: 0}`
+                );
+              }
+            });
+          }
+        });
   }
 }, 500);
-
-
-function hide() {
-  getDefaultOptions()
-      .then((options) => {
-        if (options[OPTIONS_KEYS.HIDE_DIFFICULTY]) {
-          Array.from(document.querySelectorAll('th')).forEach((th, index) => {
-            const text = th.innerText.toLowerCase();
-            console.log(text);
-            if (text === 'acceptance' || text === 'difficulty') {
-              document.styleSheets[0].insertRule(`tr td:nth-of-type(${index + 1}) {font-size: 0}`);
-            }
-          });
-        }
-      });
-}
